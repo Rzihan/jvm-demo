bfbedf5aa95739650b198ad09df452be84c5fa88
zihanPan <470590444@qq.com>
2019-08-24 16:35:14 +0800

    分析字节码对于异常的处理情况

---
+++ src/main/java/com/pzh/bytecode/AnalyzeExceptionClassDemo.java
@@ -0,0 +1,52 @@
+package com.pzh.bytecode;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+
+/**
+ * 分析字节码的异常处理的demo
+ * @author Pan梓涵
+ * @date 2019/8/9 16:40
+ */
+public class AnalyzeExceptionClassDemo {
+
+    /**
+     * 对于Java类种的每一个实例方法（非static）方法，其在编译后所生成的字节码当中，方法参数的数量总是会比源代码中方法
+     * 参数的数量多一个（this），它位于方法的第一个参数位置处；这样，我就就可以在Java的实例方法中使用this来去访问当前
+     * 对象的属性以及其他方法
+     *
+     * 这个操作是在编译期间完成的，即由javac编译器在编译的时候将对this的访问转化为对一个普通实例方法参数的访问；接下来
+     * 在运行期间，由jvm在调用实例方法时，自动向实例方法传入该this参数。所以，在实例方法的局部变量表中，至少会有一个指向
+     * 当前对象的局部变量。
+     *
+     * Java字节码对于异常的处理方式:
+     * 1、统一采用异常表的方式来对异常进行处理。
+     * 2、在jdk 1.4.2之前的版本中，并不是使用异常表的方式来对异常进行那个处理的，而是采用特定的指令方式。
+     * 3、当异常处理存在finally语句块时，现代化的JVM采取的处理方式是将finally语句块的字节码拼接到每一个catch块后面，
+     *    换句话说，程序存在多少个catch块，就会在每一个catch块后面重复多少个finally语句块的字节码。
+     */
+    public void test() {
+        try {
+            InputStream inputStream = new FileInputStream("test.txt");
+            ServerSocket serverSocket = new ServerSocket(9999);
+            serverSocket.accept();
+        } catch (FileNotFoundException fileNotFound) {
+
+        } catch (IOException ioe) {
+
+        } catch (Exception e) {
+
+        } finally {
+            System.out.println("finally");
+        }
+    }
+
+    public void testThrowsException() throws IOException, NullPointerException {
+        InputStream inputStream = new FileInputStream("test.txt");
+        ServerSocket serverSocket = new ServerSocket(9999);
+        serverSocket.accept();
+    }
+}
